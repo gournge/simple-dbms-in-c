882888fcdcc09d6403d9022b639ced5bcb6956c6
Filip Morawiec <filip.morawiec@gmail.com>
2026-02-05 21:21:35 +0100

    feat: split a query by words

--- src/parser.c
+++ src/parser.c
@@ -1,7 +1,21 @@
+#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "parser.h"
+#include "utils.h"
 
 Token* parse_query(const char* query) {
+
+    printf("Parsing query: %s\n", query);
+    int word_count = 0;
+    char** words = split_string(query, ' ', &word_count);
+
+    for (int i = 0; i < word_count; i++) {
+        printf("Word %d: %s\n", i, words[i]);
+        free(words[i]);
+    }
+    free(words);
+
     // Simple placeholder implementation for parsing
     Token* root = (Token*)malloc(sizeof(Token));
     root->type = TOKEN_ROOT;
--- tests/test_parser.c
+++ tests/test_parser.c
@@ -1,6 +1,15 @@
 #include <assert.h>
+#include <stdlib.h>
 #include "parser.h"
 
 void test_parser() {
-    assert(1 + 1 == 2);
+    test_parse_query_non_null();
 }
+
+void test_parse_query_non_null() {
+    const char* query = "SELECT * FROM Clients;";
+    Token* root = parse_query(query);
+    assert(root != NULL);
+    assert(root->type == TOKEN_ROOT);
+}
+
